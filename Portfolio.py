e642a286f07551b0d11eebf535ee440d554b2971
kubra Kandemir <48560901+kandemirkubra@users.noreply.github.com>
2021-03-12 22:56:35 +0300

    Portfolio.py dosyasına yorum satırı eklendi

--- Portfolio.py
+++ Portfolio.py
@@ -1,40 +1,46 @@
 import tkinter
 
+# isWorking uygulamanin acik olma durumu
 isWorking = False
 
+# portfolio daki itemlari listeye listeleyecek fonksiyon
 def GetPortfolioItems():
 
     itemList.insert(tkinter.END, "TSLA")
     itemList.insert(tkinter.END, "MSFL")
     itemList.insert(tkinter.END, "MCRSFT")
 
-
+# portfolio nun toplam degerini hesaplayacak fonksiyon
 def CalculateValue():
 
     null
 
-
+# portfolio ya belirtilen item i ekleyecek fonksiyon
 def AddItem():
 
     null
-    
+
+# listedeki seçili item in arayüzünü açacak fonksiyon
 def EditItem():
 
     null
-    
+
+# listedeki seçili item i portfolio dan çikaracak olan fonksiyon 
 def RemoveItem():
 
     null
 
-
+# degeri arayüz için alan fonksiyon
 def GetValue():
 
     return '0'
 
+# uygulamanýn alim satimini baslatan fonksiyon
 def Start():
 
     global isWorking
 
+    # global deki isWorking degerini degistir arayüzü ona göre güncelle
     if isWorking:
         isWorking = False
         StartButton.config(text = "Start")
@@ -47,81 +53,92 @@ def Start():
     print(isWorking)
 
 
+# Tkinter arayüz kurulumu
 main = tkinter.Tk()
 main.title("Portfolio")
 main.resizable(False, False)
 
 
-
+# Tema renkleri belirleme kismi
 themeColor = "gray"
 systemColor = "black"
 userColor = "white"
 
 
-
+# arayüzün boyutlari
 m_height = 100
 m_width = 200
 
 
-
+# arayüz iç boyutlari
 sideSpace = 50
 topBottomSpace = 125
 
-
+# arayüzün tamami
 mainFrame = tkinter.Frame(main, bg = themeColor, height = m_height, width = m_width)
 mainFrame.pack(side = tkinter.LEFT, fill = tkinter.Y)
 
-
+# arayüzün baslik kismi
 titleFrame = tkinter.Frame(mainFrame, bg = themeColor, width = m_width, height = 30)
 titleFrame.pack(side = tkinter.TOP, fill = tkinter.X)
 
+# arayüzün alt kismi
 bottomFrame = tkinter.Frame(mainFrame, bg = themeColor, width = m_width, height = 30)
 bottomFrame.pack(side = tkinter.BOTTOM, fill = tkinter.X)
 
+# liste için scrollbar kismi
 scrollbar = tkinter.Scrollbar(mainFrame)
 scrollbar.pack(side = tkinter.LEFT, fill = tkinter.Y)
 
-
+# item larin listelenecegi yer
 itemList = tkinter.Listbox(mainFrame, bg = themeColor, width = 30,font = 20, height = 10, yscrollcommand = scrollbar.set, justify = tkinter.CENTER)
 itemList.pack()
 
-
+# kenar alani için arayüz boyutlari
 s_height= 100
 s_width = 200
 
+# kenar alani (tuslarin alani)
 sideFrame = tkinter.Frame(main, bg = themeColor, height = s_height, width = s_width)
 sideFrame.pack(side = tkinter.RIGHT , fill = tkinter.Y)
 
+# iki alani bölecek olan renk katmani
 divider = tkinter.Frame(main, bg = "black", height = s_height, width = 2)
 divider.pack(side = tkinter.RIGHT, fill = tkinter.Y)
 
+#item alanina gelecek placeholder texti ve item text ini
 itemName = tkinter.StringVar()
 itemName.set('Item Shortcut')
 
+# deger gösterilmesi için veriye ataniyor
 valueText = tkinter.StringVar()
 valueText.set('Value: ' + GetValue())
 
+# item adi girme kismi
 EntryField = tkinter.Entry(sideFrame, bg = themeColor, fg = userColor, font = 24, textvariable = itemName, justify = tkinter.CENTER)
 EntryField.pack(side = tkinter.TOP)
 
+# item ekleme tusu
 addItemButton = tkinter.Button(sideFrame, bg = themeColor, fg = systemColor, text = "Add Item", font = 24, width = 10, height = 2, command = AddItem)
 addItemButton.pack(side = tkinter.TOP)
 
+# item düzenleme tusu
 editItemButton = tkinter.Button(sideFrame, bg = themeColor, fg = systemColor, text = "Edit Item", font = 24, width = 10, height = 2, command = EditItem)
 editItemButton.pack(side = tkinter.TOP)
 
+# item silme tusu
 removeItemButton = tkinter.Button(sideFrame, bg = themeColor, fg = systemColor, text = "Remove Item", font = 24, width = 10, height = 2, command = RemoveItem)
 removeItemButton.pack(side = tkinter.TOP)
 
-
+# degeri gösterecek olan alan
 valueLabel = tkinter.Label(titleFrame, bg = themeColor, fg = userColor, font = 18, text = valueText.get())
 valueLabel.pack()
 
-
+# baslat tusu
 StartButton = tkinter.Button(sideFrame, bg = themeColor, fg = systemColor, text = "Start", font = 24, width = 10, height = 2, command = Start)
 StartButton.pack(side = tkinter.TOP)
 
-
+# Portfolio itemlarini basladigin gibi çek
 GetPortfolioItems()
 
 
